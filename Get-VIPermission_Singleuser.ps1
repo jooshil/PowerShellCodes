53849503dc535deba37d3443da30baaa0b32d9c2
Jooshil Pilakkattu <jooshil.p@gmail.com>
2022-07-07 13:33:29 +0800

    Get permissits initial commit

---
+++ Get-VIPermission_Singleuser.ps1
@@ -0,0 +1,60 @@
+
+<#
+.SYNOPSIS
+    Get-VIPermission-ForSignleUser
+.DESCRIPTION
+    this scrpt extacts the vcenter rols and permission for a signle ser from list of VC's
+.NOTES
+    provide the list of vcenters with user name and password in a csv file 
+.LINK
+ 
+#>
+
+
+
+
+$vclist = Import-Csv "C:\Scripts\VMC_Scripts\vmcvclist.csv"
+
+
+foreach ($item in $vclist) {
+
+Write-Host "checking on host" $item.vc
+    Connect-VIServer $item.vc -User $item.user -Password $item.pass
+
+    #$userpermission = Get-VIPermission | ? { ($_.Principal -like "*svcvraadadmin*") -or ($_.Principal -like "*svcvraadmin*") }
+
+    $userpermission = Get-VIPermission | ? { ($_.Principal -like "*PRV_AIGGS_VMW_Build*") -or ($_.Principal -like "*PRV_AIGGS_VMW_Build*") }
+
+    
+
+
+    $userpermission | select @{N = "VCName"; E = { $item.vc } }, Principal, Role, Propagate, Entity | Export-Csv .\UserPermissionDataBuild.csv -NoTypeInformation -Append -Delimiter ","
+
+    Disconnect-VIServer *   -Force:$true -Confirm:$false
+
+
+}
+
+
+# Remove Permission
+
+
+
+foreach ($item in $vclist) {
+
+    Write-Host "checking on host" $item.vc
+        Connect-VIServer $item.vc -User $item.user -Password $item.pass
+    
+        $userpermission = Get-VIPermission | ? {($_.Principal -like "*svcvraadmin*") -and ($_.Role -like "*ReadOnly*") }
+
+        
+    
+        Remove-VIPermission -Permission  $userpermission -Confirm:$false
+
+        #$userpermission | select @{N = "VCName"; E = { $item.vc } }, Principal, Role, Propagate, Entity | Export-Csv .\UserPermissionDataRORemoval.csv -NoTypeInformation -Append -Delimiter ","
+    
+        #Disconnect-VIServer *   -Force:$true -Confirm:$false
+    
+    
+    }
+
